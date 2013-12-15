1def95ffb5f486fdeaca2dcfbf0701abda6af379
icebox <albertosantini@gmail.com>
2013-12-15 09:13:16 +0100

    Reformat app.js.

--- app/js/app.js
+++ app/js/app.js
@@ -2,24 +2,24 @@
     "use strict";
 
     var app = angular.module("piadinamia", [
-            "firebase",
-            "ui.bootstrap",
-            "ngRoute"
-        ]);
+        "firebase",
+        "ui.bootstrap",
+        "ngRoute"
+    ]);
 
     app.constant("FBURL", "https://piadinamia.firebaseio.com/");
 
     app.config(["$routeProvider", function ($routeProvider) {
         $routeProvider
-        .when("/", { templateUrl: "views/default.html" })
-        .when("/signin", { templateUrl: "views/users/signin.html" })
-        .when("/signup", { templateUrl: "views/users/signup.html" })
-        .otherwise({ redirectTo: "/" });
+            .when("/", { templateUrl: "views/default.html" })
+            .when("/signin", { templateUrl: "views/users/signin.html" })
+            .when("/signup", { templateUrl: "views/users/signup.html" })
+            .otherwise({ redirectTo: "/" });
     }]);
 
     app.run(["sessionService", "$rootScope", "FBURL",
         function (sessionService, $rootScope, FBURL) {
-        $rootScope.auth = sessionService.init("/signin");
-        $rootScope.FBURL = FBURL;
-    }]);
+            $rootScope.auth = sessionService.init("/signin");
+            $rootScope.FBURL = FBURL;
+        }]);
 }());
