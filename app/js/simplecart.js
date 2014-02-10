bb0e39212569e103414bfe14549ea46c7a8d452a
icebox <albertosantini@gmail.com>
2014-02-10 22:55:19 +0100

    Add standalone simple cart js.

---
+++ app/js/simplecart.js
@@ -0,0 +1,43 @@
+/*global $, simpleCart */
+
+(function () {
+    "use strict";
+
+    simpleCart({
+        checkout: {
+            type: "custom",
+            fn: function () {
+                // myCheckout(this);
+            }
+        },
+        currency: "EUR",
+        cartColumns: [
+            {attr: "name", label: "Item"},
+            {view: "currency", attr: "price", label: "Price"},
+            {attr: "quantity", label: "Qty"},
+            {view: "increment", label: false},
+            {view: "decrement", label: false},
+            {view: "currency", attr: "total", label: "Total" },
+            {view: "remove", text: "Remove ", label: false}
+        ],
+        cartStyle: "table"
+    });
+
+    simpleCart.currency({
+        code: "EUR",
+        name: "EUR",
+        symbol: "&euro;",
+        delimiter: " ",
+        decimal: ",",
+        after: true,
+        accuracy: 2
+    });
+
+    simpleCart.bind("update", function () {
+        if (simpleCart.quantity() === 0) {
+            $(".simpleCart_items").hide();
+        } else {
+            $(".simpleCart_items").show();
+        }
+    });
+}());
