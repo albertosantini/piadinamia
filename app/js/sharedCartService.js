942f20df66928b5e0f6752ec80c34156bf7cc25f
icebox <albertosantini@gmail.com>
2014-04-24 20:36:58 +0200

    Add getCartByUser in shared cart.

--- app/js/sharedCartService.js
+++ app/js/sharedCartService.js
@@ -1,9 +1,10 @@
 "use strict";
 
 angular.module("piadinamia").factory("sharedCartService",
-    ["$firebase", "Firebase", "FBURL",
-    function ($firebase, Firebase, FBURL) {
+    ["cartService", "$firebase", "Firebase", "FBURL",
+    function (cartService, $firebase, Firebase, FBURL) {
         var myCustomers = [],
+            myCatalogName,
             fburl;
 
         return {
@@ -11,16 +12,18 @@ angular.module("piadinamia").factory("sharedCartService",
                 var customersRef;
 
                 myCustomers = customers;
+                myCatalogName = catalogName;
+
                 fburl = FBURL + "/users/" + userId +
                     "/catalogs/" + catalogName + "/customers";
 
                 customersRef = new Firebase(fburl);
 
-                $firebase(customersRef).$on("change", function() {
+                $firebase(customersRef).$on("change", function () {
                     $firebase(customersRef).$on("loaded", function (customers) {
                         myCustomers = [];
 
-                        angular.forEach(customers, function(item) {
+                        angular.forEach(customers, function (item) {
                             myCustomers.push(item);
                         });
                     });
@@ -29,7 +32,28 @@ angular.module("piadinamia").factory("sharedCartService",
 
             getCartByItem: function () {},
 
-            getCartByUser: function () {}
+            getCartByUser: function () {
+                var cartByUser = {};
+
+                myCustomers.forEach(function (userId) {
+                    var cartRef,
+                        url = FBURL + "/users/" + userId +
+                            "/catalogs/" + myCatalogName + "/cart",
+                        myCart;
+
+                    cartRef = new Firebase(url);
+                    $firebase(cartRef).$on("loaded", function (cart) {
+                        myCart = [];
+
+                        angular.forEach(cart, function (item) {
+                            myCart.push(item);
+                        });
+
+                        cartByUser[userId] = myCart;
+                    });
+
+                });
+            }
 
         };
 
