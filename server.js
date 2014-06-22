7f87f5fe6b731f2a00c59273f51a0a4e17becdb4
icebox <albertosantini@gmail.com>
2014-06-22 10:20:58 +0200

    Review server script.

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 "use strict";
 
-var path = require("path");
-
-var express = require("express"),
-    favicon = require("static-favicon");
+var path = require("path"),
+    express = require("express");
 
 var app = express(),
     port = 8000,
     pubdir = path.join(__dirname, "/app");
 
 app.use(express.static(pubdir));
-app.use(favicon(__dirname + pubdir + "/favicon.ico"));
 
 app.listen(port, function () {
     console.log("piadinamia listening on port %s", port);
