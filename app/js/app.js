1cdb457c140da70efb0aaf2c2e2355f33ec83812
icebox <albertosantini@gmail.com>
2014-10-15 20:28:06 +0200

    Refactor app.js with Papa's style guide.

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,25 @@
 (function () {
     "use strict";
 
-    var app = angular.module("piadinamia", [
-        "firebase",
-        "ui.bootstrap",
-        "ngRoute"
-    ]);
+    angular
+        .module("piadinamia", [
+            "firebase",
+            "ui.bootstrap",
+            "ngRoute"
+        ])
+        .constant("FBURL", "https://piadinamia.firebaseio.com/")
+        .config(config);
 
-    app.constant("FBURL", "https://piadinamia.firebaseio.com/");
+    config.$inject = ["$routeProvider", "$locationProvider"];
 
-    app.config(
-        ["$routeProvider", "$locationProvider",
-        function ($routeProvider, $locationProvider) {
-            $routeProvider
-                .when("/", {templateUrl: "views/default.html"})
-                .when("/signin", {templateUrl: "views/users/signin.html"})
-                .when("/signup", {templateUrl: "views/users/signup.html"})
-                .otherwise({redirectTo: "/"});
-            $locationProvider.html5Mode(true);
-    }]);
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/", {templateUrl: "views/default.html"})
+            .when("/signin", {templateUrl: "views/users/signin.html"})
+            .when("/signup", {templateUrl: "views/users/signup.html"})
+            .otherwise({redirectTo: "/"});
+
+        $locationProvider.html5Mode(true);
+    }
 
 }());
