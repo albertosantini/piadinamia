dc18f2e3228325ff7028388abe52f16096124596
Alberto Santini <albertosantini@gmail.com>
2014-10-17 10:02:16 +0200

    Refactor catalogService.js with Papa's style guide.

--- app/js/catalogService.js
+++ app/js/catalogService.js
@@ -1,8 +1,20 @@
-"use strict";
+(function () {
+    "use strict";
 
-angular.module("piadinamia").factory("catalogService",
-    ["$firebase", "Firebase", "FBURL", "$q",
-    function ($firebase, Firebase, FBURL, $q) {
+    angular
+        .module("piadinamia")
+        .factory("catalogService", catalogService);
+
+    catalogService.$inject = ["$firebase", "Firebase", "FBURL", "$q"];
+
+    function catalogService($firebase, Firebase, FBURL, $q) {
+        var service = {
+            load: load,
+            search: search,
+            onSelect: onSelect
+        };
+
+        return service;
 
         function getMaster(query) {
             var master = new Firebase(FBURL + "/master"),
@@ -24,72 +36,72 @@ angular.module("piadinamia").factory("catalogService",
             return deferred.promise;
         }
 
-        return {
-            load: function (id, callback) {
-                var ref = new Firebase(FBURL + "/users/" + id + "/catalogs"),
-                    catsSync = $firebase(ref).$asObject(),
-                    defaultCatName = {
-                        name: "piadinamia"
+        function load(id, callback) {
+            var ref = new Firebase(FBURL + "/users/" + id + "/catalogs"),
+                catsSync = $firebase(ref).$asObject(),
+                defaultCatName = {
+                    name: "piadinamia"
+                },
+                defaultCat = {
+                    name: "piadinamia",
+                    description: "Piadina Mia",
+                    private: false,
+                    items: {
+                        "Piadina crudo": 4.00,
+                        "Piadina farcita": 4.80,
+                        "Piadina con nutella": 3.00,
+                        "Piadina integrale crudo": 4.80,
+                        "Acqua naturale": 1.50,
+                        "Acqua frizzante": 2.00,
+                        "Bibita": 2.50
                     },
-                    defaultCat = {
-                        name: "piadinamia",
-                        description: "Piadina Mia",
-                        private: false,
-                        items: {
-                            "Piadina crudo": 4.00,
-                            "Piadina farcita": 4.80,
-                            "Piadina con nutella": 3.00,
-                            "Piadina integrale crudo": 4.80,
-                            "Acqua naturale": 1.50,
-                            "Acqua frizzante": 2.00,
-                            "Bibita": 2.50
-                        },
-                        cart: [
-                            {
-                                item: "Piadina crudo",
-                                price: 4.00,
-                                qty: 2.00,
-                                total: 8.00
-                            }
-                        ],
-                        subscribers: [
-                            {
-                                id: id
-                            }
+                    cart: [
+                        {
+                            item: "Piadina crudo",
+                            price: 4.00,
+                            qty: 2.00,
+                            total: 8.00
+                        }
+                    ],
+                    subscribers: [
+                        {
+                            id: id
+                        }
 
-                        ]
-                    };
+                    ]
+                };
 
-                catsSync.$loaded().then(function () {
-                    if (catsSync.$value === "" || catsSync.$value === null) {
-                        catsSync.default = defaultCatName;
-                        catsSync.piadinamia = defaultCat;
+            catsSync.$loaded().then(function () {
+                if (catsSync.$value === "" || catsSync.$value === null) {
+                    catsSync.default = defaultCatName;
+                    catsSync.piadinamia = defaultCat;
 
-                        catsSync.$save().then(function () {
-                            callback(defaultCat, "piadinamia");
-                        });
-                    } else {
-                        callback(catsSync[catsSync.default.name],
-                            catsSync.default.name);
-                    }
-                });
-            },
-
-            search: function (query) {
-                return getMaster(query).then(function (catalogs) {
-                    var cats = [];
-
-                    angular.forEach(catalogs, function (userId, description) {
-                        cats.push(description);
+                    catsSync.$save().then(function () {
+                        callback(defaultCat, "piadinamia");
                     });
+                } else {
+                    callback(catsSync[catsSync.default.name],
+                        catsSync.default.name);
+                }
+            });
+        }
 
-                    return cats;
+        function search(query) {
+            return getMaster(query).then(function (catalogs) {
+                var cats = [];
+
+                angular.forEach(catalogs, function (userId, description) {
+                    cats.push(description);
                 });
-            },
 
-            onSelect: function ($item, $model, $label) {
-                console.log($item, $model, $label);
-            }
+                return cats;
+            });
+        }
 
-        };
-    }]);
+        function onSelect($item, $model, $label) {
+            console.log($item, $model, $label);
+        }
+
+    }
+
+}());
