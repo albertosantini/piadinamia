1e62cf75f1a32a9cbfcc1c9f3c6d930bc3eca07a
icebox <albertosantini@gmail.com>
2018-05-04 23:31:23 +0200

    Add local server vs. firebase deploy.
    Remove firebase deploy.

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "./src/client")));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+
+app.listen(8000, () => {
+    console.log("Listening on http://localhost:8000");
+});
