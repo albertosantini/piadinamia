39e7a36eed4e028869b6d8b859bffeb65cac4700
icebox <albertosantini@gmail.com>
2018-05-05 08:44:14 +0200

    Redirect requests to root.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use("/", express.static(path.join(__dirname, "./src/client")));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
+app.use((req, res) => {
+    res.redirect("/");
+});
+
 app.listen(8000, () => {
     console.log("Listening on http://localhost:8000"); /* eslint no-console:off */
 });
